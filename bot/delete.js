60351d2c3df5c6ecd543c62efd3015bcc686af4e
andole87 <andole87@gmail.com>
2019-05-11 16:18:47 +0900

    refactor: Api-token 숨김

--- bot/delete.js
+++ bot/delete.js
@@ -1,10 +1,11 @@
 const axios = require('axios')
+const token = require('./api_info').token
 
 axios.post('https://slack.com/api/channels.delete',{
-    "token":"token",
-    "channel":"CJL4EQUDS"
+    "token": token,
+    "channel": "CJL4EQUDS"
 })
-.then((res) => {
+    .then((res) => {
     console.log(res);
 })
 .catch((err) =>{
