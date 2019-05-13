b505f16ba7e16086c8befe5c5534a41b4908b9ab
andole87 <andole87@gmail.com>
2019-05-13 23:36:58 +0900

    feat: 상호작용 가능한 버튼 HTTP POST
    Incoming Webhooks로 먼저 버튼이 포함된 메시지를 보낸다.
    포스트된 버튼을 누르면 public_ip:80 으로 !!POST!!가 온다.
    express가 POST 바디를 !!!body-parser!!!로 바꿔줘야 한다.
    request payload json을 파싱하면 된다...
    
    Oauth2 한다고 삽질했는데, 퍼블릭 앱으로 배포할 거 아니면 안해도 된다.
    개삽질했는데..................................4시간 날려따리 날려따.
    
    기타 설정은 https://api.slack.com에서 해줘야 한다. interactive 가능,
    콜백, response request url 잡아줘야 한다.
    incoming webhook도 한 앱이 동시에 가질 수 있다. 인터레스팅.

---
+++ interactive-components/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const axios = require('axios')
+const api_info = require('./api_info')
+const message = require('./post_data')
+const bodyParser = require('body-parser')
+
+const app = express()
+const parser = bodyParser.urlencoded({extended:false})
+
+axios.post(api_info.post_url, message)
+.then((res) => {
+    console.log(`status: ${res.statusCode}`)
+})
+.catch((error) => {
+    console.error(error)
+})
+
+app.post('/button',parser,(req,res) =>{
+    console.log(req.body)
+    res.send('something?')
+})
+app.listen(80,()=>{
+    console.log('opened')
+})
